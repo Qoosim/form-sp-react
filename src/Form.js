7c9320a76283079c1ecc2cb16e36340af5d4eb7c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-12-15 21:25:19 +0100

    Add form validation to the form

--- src/Form.js
+++ src/Form.js
@@ -27,8 +27,9 @@ function Form() {
  return (
     <div className="form-container">
       <form className="register-form" onSubmit={handleSubmit}>
-        {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
+
+        {submitted && <div className="success-message">Success! Thank you for registering</div>}
+
         <input
           id="first-name"
           className="form-field"
@@ -38,8 +39,9 @@ function Form() {
           value={values.firstName}
           onChange={handleChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
+
+        {submitted && !values.firstName && <span id="first-name-error">Please enter a first name</span>}
+
         <input
           id="last-name"
           className="form-field"
@@ -49,8 +51,9 @@ function Form() {
           value={values.lastName}
           onChange={handleChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
+
+        {submitted && !values.lastName && <span id="last-name-error">Please enter a last name</span>}
+
         <input
           id="email"
           className="form-field"
@@ -60,15 +63,13 @@ function Form() {
           value={values.email}
           onChange={handleChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
+
+        {submitted && !values.email && <span id="email-error">Please enter an email address</span>}
+
         <button className="form-field" type="submit">
           Register
         </button>
       </form>
-
-      {submitted && <div className='success-message'>Success! Thank you for registering</div>}
-
     </div>
   );
 }
